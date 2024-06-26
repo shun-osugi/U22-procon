b3fe0e27f83e0183d7ed4a8c3da392a89a7ebd6d
Genki Otake <231205037@ccmailg.meijo-u.ac.jp>
2024-06-26 20:20:43 +0900

    Add header & Footer page.

--- u_22_procon/lib/main.dart
+++ u_22_procon/lib/main.dart
@@ -1,125 +1,95 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:u_22_procon/samplePage.dart';
+//遷移先ファイルのインポート文を記述
+//下が例
+// import 'package:XXX/page_a.dart';
 
-void main() {
-  runApp(const MyApp());
+//main関数
+main() {
+  //アプリ
+  const app = MaterialApp(home: HeaderFooter());
+
+  //プロバイダースコープでアプリを囲む
+  const scop = ProviderScope(child: app);
+  runApp(scop);
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+//プロバイダー
+final indexProvider = StateProvider((ref) {
+  //変化するデータ　0, 1, 2...(遷移先)
+  return 1; //時間割から
+});
 
-  // This widget is the root of your application.
+//画面
+class HeaderFooter extends ConsumerWidget {
+  const HeaderFooter({super.key});
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+  Widget build(BuildContext context, WidgetRef ref) {
+    //インデックス
+    final index = ref.watch(indexProvider);
+
+    //ヘッダー
+    final header = AppBar(
+      title: const Text('時間割'), //ヘッダーのテキストは後日再考する
+      backgroundColor: Colors.grey[350], //背景色
     );
-  }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    //フッターアイテムたち
+    const footerItems = [
+      //画面1(仮)
+      BottomNavigationBarItem(
+        icon: Icon(
+          Icons.format_list_bulleted_add,
+          size: 45.0,
         ),
+        label: '',
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      //画面2(仮)
+      BottomNavigationBarItem(
+        icon: Icon(
+          Icons.calendar_month,
+          size: 45,
+        ),
+        label: '',
+      ),
+      //画面3(仮)
+      BottomNavigationBarItem(
+        icon: Icon(
+          Icons.menu_book,
+          size: 45,
+        ),
+        label: '',
+      ),
+    ];
+
+    //footer
+    final footerBar = BottomNavigationBar(
+      items: footerItems, //フッターアイテムたち
+      backgroundColor: Colors.grey[350], //背景色
+      selectedItemColor: Colors.cyan, //選択されたアイテムの色
+      unselectedItemColor: Colors.black, //選択せれていない時のアイテムの色
+      currentIndex: index, //インデックス
+      //タップされた時インデックス(画面)を変更する
+      onTap: (index) {
+        ref.read(indexProvider.notifier).state = index;
+      },
+    );
+
+    //遷移先の画面たち
+    const pages = [
+      //ここに遷移先の画面を記述
+      Samplepage(),
+      //例：
+      //PageA(),
+      //PageB(),
+    ];
+
+    return Scaffold(
+      appBar: header,
+      body: pages[index],
+      bottomNavigationBar: footerBar,
     );
   }
 }
---
+++ u_22_procon/lib/samplePage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Samplepage extends StatelessWidget {
+  const Samplepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.pink,
+      body: const Center(
+        child: Text('サンプルページ'),
+      ),
+    );
+  }
+}
