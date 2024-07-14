38f0d2a8208a5a9553c967bf43932763c1450730
shun <shun01.oh@gmail.com>
2024-07-14 19:02:20 +0900

    add #29 サンプルページ3から専門用語集へのページ遷移

--- u_22_procon/lib/samplePage3.dart
+++ u_22_procon/lib/samplePage3.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'tech_term.dart'; // tech_term.dartのインポート
 
 class Samplepage3 extends StatelessWidget {
   const Samplepage3({super.key});
@@ -10,6 +11,17 @@ class Samplepage3 extends StatelessWidget {
       body: const Center(
         child: Text('サンプルページ3'),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => TechTermPage()),
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.endFloat, // ボタンの位置を右下に設定
     );
   }
 }
