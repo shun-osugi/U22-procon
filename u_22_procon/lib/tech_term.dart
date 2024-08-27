85f7147efea5186e2db77dbb8be58f7e5eb9e206
shun-osugi <146703013+shun-osugi@users.noreply.github.com>
2024-08-27 23:56:43 +0900

    Update tech_term.dart

--- u_22_procon/lib/tech_term.dart
+++ u_22_procon/lib/tech_term.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'firebase_options.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class TechTermPage extends StatefulWidget {
